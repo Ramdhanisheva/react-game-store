04bda440a3961c30413c1e52f7e6aba6631d7587
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-02-27 22:49:28 +0100

    Added Tanstack Query client

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvider } from "./context/AuthContext";
 import { FirestoreContextProvider } from "./context/FirestoreContext";
 import { CartContextProvider } from "./context/CartContext";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <AuthContextProvider>
-        <FirestoreContextProvider>
-          <CartContextProvider>
-            <App />
-          </CartContextProvider>
-        </FirestoreContextProvider>
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <FirestoreContextProvider>
+            <CartContextProvider>
+              <App />
+            </CartContextProvider>
+          </FirestoreContextProvider>
+        </AuthContextProvider>
+      </QueryClientProvider>
     </HashRouter>
   </React.StrictMode>
 );
