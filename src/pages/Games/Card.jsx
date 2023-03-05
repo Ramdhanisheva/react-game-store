9dc7b235337e1b9a94c8e8aa7c589ed3af91f7c2
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-05 10:23:06 +0100

    Added transition Card components

--- src/pages/Games/Card.jsx
+++ src/pages/Games/Card.jsx
@@ -1,18 +1,19 @@
-import {
-  FaXbox,
-  FaPlaystation,
-  FaWindows,
-  FaPlus,
-  FaHeart,
-} from "react-icons/fa";
+import { motion } from "framer-motion";
 import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import { FaCheck } from "react-icons/fa";
+import {
+  FaCheck,
+  FaHeart,
+  FaPlaystation,
+  FaPlus,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
 import { SiNintendoswitch } from "react-icons/si";
-import getPrice from "../../utils/getPrice";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import { CartContext } from "../../context/CartContext";
 import { FirestoreContext } from "../../context/FirestoreContext";
-import { Link } from "react-router-dom";
+import getPrice from "../../utils/getPrice";
 
 const Card = ({
   id,
@@ -44,6 +45,11 @@ const Card = ({
     arr.length - 1 == index ? `${genre.name}` : `${genre.name}, `
   );
 
+  const animation = {
+    in: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
   const platform = {
     PC: null,
     PlayStation: null,
@@ -66,68 +72,79 @@ const Card = ({
       className="card card-compact col-span-12 md:col-span-6 lg:col-span-4 2xl:col-span-3 h-fit bg-zinc-800 shadow-xl transition-all hover:bg-zinc-700/[.55] hover:scale-[1.02] duration-300"
       key={id}
     >
-      <figure className="relative aspect-[16/9] max-h-min overflow-hidden">
-        <Link to={`/games/${id}`}>
-          <img src={image} alt={name} />
-        </Link>
-        <button
-          className={
-            !isLoading && firestoreState.wishlist
-              ? firestoreState.wishlist.find(
-                  (game) => game.data().name === name
-                )
-                ? "p-3 bg-black absolute top-3 right-3 rounded-full text-red-500 transition-colors"
-                : "p-3 bg-black absolute top-3 right-3 rounded-full text-white transition-all active:scale-90 hover:text-red-500"
-              : "p-3 bg-black absolute top-3 right-3 rounded-full text-white btn-disabled"
-          }
-          onClick={() => handleHeartClick(obj, "wishlist", name)}
-        >
-          <FaHeart className=" text-sm" />
-        </button>
-      </figure>
-      <div className="p-5">
-        <div className="flex justify-between text-white py-1">
-          {!firestoreState.isLoading &&
-          firestoreState.cartItems &&
-          firestoreState.cartItems
-            .data()
-            .games.find((game) => game.name === name) ? (
-            <div className="flex items-center text-success transition-colors cursor-default">
-              <span className="text-sm font-semibold mr-2">Added</span>
-              <FaCheck />
-            </div>
-          ) : (
-            <div
-              className="flex text-zinc-400 hover:text-primary transition-colors duration-200 cursor-pointer"
-              onClick={() => handleCartClick("orders", "add", obj)}
-            >
-              <span className="text-sm font-semibold  mr-2 self-center">
-                Add to cart
-              </span>
-              <FaPlus className="self-center" />
-            </div>
-          )}
-          <span className="font-semibold">${getPrice(name)}</span>
-        </div>
-        <div className="flex justify-between gap-2">
-          <span className="card-title text-white font-bold">{name}</span>
-          <div className={metacriticStyles}>
-            <span className="text-xs">{metacritic}</span>
+      <motion.div
+        variants={animation}
+        initial={"in"}
+        animate={"animate"}
+        className="rounded-xl"
+      >
+        <figure className="relative aspect-[16/9] max-h-min overflow-hidden">
+          <Link to={`/games/${id}`}>
+            <img src={image} alt={name} />
+          </Link>
+          <button
+            className={
+              !isLoading && firestoreState.wishlist
+                ? firestoreState.wishlist.find(
+                    (game) => game.data().name === name
+                  )
+                  ? "p-3 bg-black absolute top-3 right-3 rounded-full text-red-500 transition-colors"
+                  : "p-3 bg-black absolute top-3 right-3 rounded-full text-white transition-all active:scale-90 hover:text-red-500"
+                : "p-3 bg-black absolute top-3 right-3 rounded-full text-white btn-disabled"
+            }
+            onClick={() => handleHeartClick(obj, "wishlist", name)}
+          >
+            <FaHeart className=" text-sm" />
+          </button>
+        </figure>
+        <div className="p-5">
+          <div className="flex justify-between text-white py-1">
+            {!firestoreState.isLoading &&
+            firestoreState.cartItems &&
+            firestoreState.cartItems
+              .data()
+              .games.find((game) => game.name === name) ? (
+              <div className="flex items-center text-success transition-colors cursor-default">
+                <span className="text-sm font-semibold mr-2">Added</span>
+                <FaCheck />
+              </div>
+            ) : (
+              <div
+                className="flex text-zinc-400 hover:text-primary transition-colors duration-200 cursor-pointer"
+                onClick={() => handleCartClick("orders", "add", obj)}
+              >
+                <span className="text-sm font-semibold  mr-2 self-center">
+                  Add to cart
+                </span>
+                <FaPlus className="self-center" />
+              </div>
+            )}
+            <span className="font-semibold">${getPrice(name)}</span>
           </div>
-        </div>
-        <div className="flex justify-between my-2 text-white">
-          <div className="flex gap-2">
-            {platform.PC && <FaWindows className="self-center" />}
-            {platform.PlayStation && <FaPlaystation className="self-center" />}
-            {platform.Xbox && <FaXbox className="self-center" />}
-            {platform.Nintendo && <SiNintendoswitch className="self-center" />}
+          <div className="flex justify-between gap-2">
+            <span className="card-title text-white font-bold">{name}</span>
+            <div className={metacriticStyles}>
+              <span className="text-xs">{metacritic}</span>
+            </div>
           </div>
+          <div className="flex justify-between my-2 text-white">
+            <div className="flex gap-2">
+              {platform.PC && <FaWindows className="self-center" />}
+              {platform.PlayStation && (
+                <FaPlaystation className="self-center" />
+              )}
+              {platform.Xbox && <FaXbox className="self-center" />}
+              {platform.Nintendo && (
+                <SiNintendoswitch className="self-center" />
+              )}
+            </div>
+          </div>
+          <ul className="text-xs text-zinc-400">
+            <li>Released: {released}</li>
+            <li>Genres: {genreList}</li>
+          </ul>
         </div>
-        <ul className="text-xs text-zinc-400">
-          <li>Released: {released}</li>
-          <li>Genres: {genreList}</li>
-        </ul>
-      </div>
+      </motion.div>
     </div>
   );
 };
