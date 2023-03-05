eb3c52ea9fb684cec48c114dce1d6af525d939b2
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-03-05 09:22:09 +0100

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Routes, Route, Link, useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import { useContext } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import Drawer from "./components/Drawer";
 import { AuthContext } from "./context/AuthContext";
+import GameDetails from "./pages/GameDetails";
+import Games from "./pages/Games";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 import Signup from "./pages/Signup";
-import Games from "./pages/Games";
-import GameDetails from "./pages/GameDetails";
-import Drawer from "./components/Drawer";
-import { motion, AnimatePresence } from "framer-motion";
 
 const App = () => {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
-  const location = useLocation()
+  const location = useLocation();
 
   const RequireAuth = ({ children }) => {
     return user ? children : navigate("/auth/login");
@@ -21,42 +21,43 @@ const App = () => {
 
   return (
     <AnimatePresence mode="wait">
-    <Routes key={location.pathname} location={location}>
-      <Route
-        path="/"
-        element={
-          <Drawer>
-            <Home />
-          </Drawer>
-        }
-      />
-      <Route path="/auth">
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-      </Route>
-      <Route path="/games">
+      <Routes key={location.pathname} location={location}>
         <Route
-          path=""
+          path="/"
           element={
-            <RequireAuth>
-              <Drawer>
-                <Games />
-              </Drawer>
-            </RequireAuth>
+            <Drawer>
+              <Home />
+            </Drawer>
           }
         />
-        <Route
-          path=":id"
-          element={
-            <RequireAuth>
-              <Drawer>
-                <GameDetails />
-              </Drawer>
-            </RequireAuth>
-          }
-        />
-      </Route>
-    </Routes>
+        <Route path="/auth">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+        <Route path="/games">
+          <Route
+            path=""
+            element={
+              <RequireAuth>
+                <Drawer>
+                  <Games />
+                </Drawer>
+              </RequireAuth>
+            }
+          />
+          <Route
+            path=":id"
+            element={
+              <RequireAuth>
+                <Drawer>
+                  <GameDetails />
+                </Drawer>
+              </RequireAuth>
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </AnimatePresence>
   );
 };
